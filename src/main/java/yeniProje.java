c5f8ee43238ca18856a7394d3d4f4036cfe95ae1
Şeyma <seyma.kznc@gmail.com>
2022-07-18 12:52:50 -0700

    second version

--- src/main/java/yeniProje.java
+++ src/main/java/yeniProje.java
@@ -1,5 +1,6 @@
 public class yeniProje {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("I did");
     }
 }
---
+++ src/main/java/yeniProje2.java
@@ -0,0 +1,2 @@
+public class yeniProje2 {
+}
