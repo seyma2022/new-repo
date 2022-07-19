55c0c15baaaf821b0d7b6c698ad9762c3cdb6c29
Şeyma <seyma.kznc@gmail.com>
2022-07-18 23:54:18 -0700

    seyma özellik2

--- src/main/java/özellik1.java
+++ src/main/java/özellik1.java
@@ -1,5 +1,7 @@
 public class özellik1 {
     public static void main(String[] args) {
         System.out.println("seyma özellik 1");
+        System.out.println("seyma özellik 2");
+        System.out.println("seyma branch ortak class");
     }
 }
