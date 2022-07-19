5ec2d0f688ed9fc2f8a022b4993817b4b43a374e
Şeyma <seyma.kznc@gmail.com>
2022-07-19 00:01:13 -0700

    hollooo

--- src/main/java/özellik1.java
+++ src/main/java/özellik1.java
@@ -2,5 +2,6 @@ public class özellik1 {
     public static void main(String[] args) {
         System.out.println("seyma özellik 1");
         System.out.println("değisiklik 1");
+        System.out.println("helooo");
     }
 }
